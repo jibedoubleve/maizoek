afc5c6fcaf8c533a0add0f75a82eebfa8e3a30b0
jibedoubleve <jibedoubleve@users.noreply.github.com>
2026-04-13 14:10:23 +0200

    (#32) Update footer repo link to mazoek

--- src/index.php
+++ src/index.php
@@ -281,7 +281,7 @@ function subtypeLabel(string $st, array $t): string {
 </div>
 
 <footer class="footer">
-    <a class="footer-repo" href="https://github.com/jibedoubleve/immoweb-search" target="_blank" rel="noopener">
+    <a class="footer-repo" href="https://github.com/jibedoubleve/mazoek" target="_blank" rel="noopener">
         <svg height="16" viewBox="0 0 16 16" width="16" fill="currentColor" aria-hidden="true">
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38
             0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13
@@ -292,7 +292,7 @@ function subtypeLabel(string $st, array $t): string {
             .51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48
             0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
         </svg>
-        jibedoubleve/immoweb-search
+        jibedoubleve/mazoek
     </a>
     <span class="footer-meta">
         <?= h($versionInfo['version']) ?> &middot; <?= h($versionInfo['date']) ?>
